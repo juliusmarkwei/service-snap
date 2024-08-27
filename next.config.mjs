24a9b434a28ab6731ac1ecb9649a50d926cf0c9e
juliusmarkwei <juliusmarkwei2000@gmail.com>
2024-08-27 15:49:41 +0000

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: true,
+	images: {
+		domains: ["res.cloudinary.com", "cdn.rareblocks.xyz", "i.pinimg.com"],
+	},
+};
 
 export default nextConfig;
