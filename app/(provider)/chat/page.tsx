7834b7e7811cb2705b1cb6cca6c5186e0fd8ce63
juliusmarkwei <juliusmarkwei2000@gmail.com>
2024-08-29 15:57:44 +0000

    fix: chat input sticky

--- app/(provider)/chat/page.tsx
+++ app/(provider)/chat/page.tsx
@@ -39,7 +39,7 @@ const Chat = () => {
 						id="login"
 						className="rounded-md col-span-2 flex justify-end"
 					>
-						<div className="relative w-12 h-12">
+						<div className="relative w-11 h-11">
 							<Image
 								src="https://i.pinimg.com/236x/3e/1d/ce/3e1dced892a602d93039ebc2a150c35f.jpg"
 								alt="Profile Image"
@@ -148,11 +148,11 @@ const Chat = () => {
 					</div>
 				</div>
 
-				<div className="bg-white p-4 flex items-center">
+				<div className="bg-white p-4 flex items-center sticky bottom-0 z-10">
 					<input
 						type="text"
 						placeholder="Type your message..."
-						className="flex-1 border rounded-full px-4 py-2 focus:outline-none"
+						className="flex-1 border rounded-full px-4 py-2 focus:outline-none text-gray-900"
 					/>
 					<button className="bg-blue-500 text-white rounded-full p-2 ml-2 focus:outline-none">
 						<svg
@@ -163,22 +163,13 @@ const Chat = () => {
 							xmlns="http://www.w3.org/2000/svg"
 							stroke="#ffffff"
 						>
-							<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-							<g
-								id="SVGRepo_tracerCarrier"
+							<path
+								d="M11.5003 12H5.41872M5.24634 12.7972L4.24158 15.7986C3.69128 17.4424 3.41613 18.2643 3.61359 18.7704C3.78506 19.21 4.15335 19.5432 4.6078 19.6701C5.13111 19.8161 5.92151 19.4604 7.50231 18.7491L17.6367 14.1886C19.1797 13.4942 19.9512 13.1471 20.1896 12.6648C20.3968 12.2458 20.3968 11.7541 20.1896 11.3351C19.9512 10.8529 19.1797 10.5057 17.6367 9.81135L7.48483 5.24303C5.90879 4.53382 5.12078 4.17921 4.59799 4.32468C4.14397 4.45101 3.77572 4.78336 3.60365 5.22209C3.40551 5.72728 3.67772 6.54741 4.22215 8.18767L5.24829 11.2793C5.34179 11.561 5.38855 11.7019 5.407 11.8459C5.42338 11.9738 5.42321 12.1032 5.40651 12.231C5.38768 12.375 5.34057 12.5157 5.24634 12.7972Z"
+								stroke="#ffffff"
+								stroke-width="2"
 								stroke-linecap="round"
 								stroke-linejoin="round"
-							></g>
-							<g id="SVGRepo_iconCarrier">
-								{" "}
-								<path
-									d="M11.5003 12H5.41872M5.24634 12.7972L4.24158 15.7986C3.69128 17.4424 3.41613 18.2643 3.61359 18.7704C3.78506 19.21 4.15335 19.5432 4.6078 19.6701C5.13111 19.8161 5.92151 19.4604 7.50231 18.7491L17.6367 14.1886C19.1797 13.4942 19.9512 13.1471 20.1896 12.6648C20.3968 12.2458 20.3968 11.7541 20.1896 11.3351C19.9512 10.8529 19.1797 10.5057 17.6367 9.81135L7.48483 5.24303C5.90879 4.53382 5.12078 4.17921 4.59799 4.32468C4.14397 4.45101 3.77572 4.78336 3.60365 5.22209C3.40551 5.72728 3.67772 6.54741 4.22215 8.18767L5.24829 11.2793C5.34179 11.561 5.38855 11.7019 5.407 11.8459C5.42338 11.9738 5.42321 12.1032 5.40651 12.231C5.38768 12.375 5.34057 12.5157 5.24634 12.7972Z"
-									stroke="#ffffff"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-								></path>{" "}
-							</g>
+							></path>
 						</svg>
 					</button>
 				</div>
