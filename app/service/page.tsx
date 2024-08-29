5fa9ab99f4d457a6ffd64df726f08a0ec7ae6b2f
juliusmarkwei <juliusmarkwei2000@gmail.com>
2024-08-29 13:21:43 +0000

    feat: rating and review to service page

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 
-const ServiceProvidersProfile = () => {
+const ServiceProviders = () => {
 	return (
 		<>
 			<section className="pt-16 bg-[#ffffffe4] min-h-screen h-auto">
 				<div className="w-full px-4 mx-auto">
 					<div className="relative flex flex-col min-w-0 break-words bg-[#f8f8f8] w-full shadow-xl rounded-lg mt-16">
-						<div className="px-6">
+						<div className="px-4">
 							<div className="flex flex-wrap justify-center relative items-center">
 								<div className="w-full px-4 flex justify-center">
 									<div className="bg-black">
@@ -15,7 +15,7 @@ const ServiceProvidersProfile = () => {
 											width={400}
 											height={300}
 											src="https://demos.creative-tim.com/notus-js/assets/img/team-2-800x800.jpg"
-											className="shadow-xl rounded-full w-52 h-52 align-middle border-none absolute left-[16%] -top-24"
+											className="shadow-xl rounded-full w-52 h-52 align-middle border-none absolute left-[19%] -top-24"
 										/>
 									</div>
 								</div>
@@ -80,7 +80,7 @@ const ServiceProvidersProfile = () => {
 								</div>
 							</div>
 
-							<div className="text-center mt-3">
+							<div className="text-center">
 								<h3 className="text-xl font-semibold leading-normal text-gray-800 mb-2">
 									@jenna-stones
 								</h3>
@@ -88,30 +88,154 @@ const ServiceProvidersProfile = () => {
 									<i className="fas fa-map-marker-alt mr-2 text-lg text-black"></i>
 									Amamoma - Cape Coast
 								</div>
-								<div className="mb-2 text-gray-600 mt-10">
+								<div className="mb-2 text-gray-600 mt-7">
+									<h3 className="text-gray-700 underline decoration-2 text-lg font-bold p-1">
+										SKILLS
+									</h3>
 									<i className="fas fa-briefcase mr-2 text-lg text-black"></i>
-									Solution Manager - Creative Tim Officer
-								</div>
-								<div className="mb-2 text-gray-600">
-									<i className="fas fa-university mr-2 text-lg text-black"></i>
-									University of Computer Science
+									Web Developer{" "}
+									<span className="text-gray-500 px-1">
+										•
+									</span>{" "}
+									Graphic Artist{" "}
+									<span className="text-gray-500 px-1">
+										•
+									</span>{" "}
+									User <br /> Experience Designer{" "}
+									<span className="text-gray-500 px-2">
+										•
+									</span>{" "}
+									Photographer
 								</div>
 							</div>
 
-							<div className="mt-10 py-10 border-t border-blueGray-200 text-center ">
-								<div className="flex flex-wrap justify-center mb-5">
-									<div className="w-full lg:w-9/12 px-4">
-										<p className="mb-4 text-lg leading-relaxed text-gray-800">
-											An artist of considerable range,
-											Jenna the name taken by
-											Melbourne-raised, Brooklyn-based
-											Nick Murphy writes, performs and
-											records all of his own music, giving
-											it a warm, intimate feel with a
-											solid groove structure. An artist of
-											considerable range.
-										</p>
+							<div className="py-6 border-t border-blueGray-200 text-center">
+								{/* rate */}
+								<div className="flex gap-2 py-1 justify-center">
+									<div className="flex items-center w-[100px] h-[20px]">
+										{/* Star 1 */}
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="currentColor"
+											viewBox="0 0 24 24"
+											className="w-5 h-5 text-yellow-400"
+										>
+											<path d="M12 17.27l5.18 3.15-1.39-6.06L22 9.24l-6.19-.53L12 2 8.19 8.71 2 9.24l4.21 4.12-1.39 6.06L12 17.27z" />
+										</svg>
+
+										{/* Star 2 */}
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="currentColor"
+											viewBox="0 0 24 24"
+											className="w-5 h-5 text-yellow-400"
+										>
+											<path d="M12 17.27l5.18 3.15-1.39-6.06L22 9.24l-6.19-.53L12 2 8.19 8.71 2 9.24l4.21 4.12-1.39 6.06L12 17.27z" />
+										</svg>
+
+										{/* Star 3 */}
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="currentColor"
+											viewBox="0 0 24 24"
+											className="w-5 h-5 text-yellow-400"
+										>
+											<path d="M12 17.27l5.18 3.15-1.39-6.06L22 9.24l-6.19-.53L12 2 8.19 8.71 2 9.24l4.21 4.12-1.39 6.06L12 17.27z" />
+										</svg>
+
+										{/* Star 4 */}
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="currentColor"
+											viewBox="0 0 24 24"
+											className="w-5 h-5 text-yellow-400"
+										>
+											<path d="M12 17.27l5.18 3.15-1.39-6.06L22 9.24l-6.19-.53L12 2 8.19 8.71 2 9.24l4.21 4.12-1.39 6.06L12 17.27z" />
+										</svg>
+
+										{/* Star 5 */}
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="currentColor"
+											viewBox="0 0 24 24"
+											className="w-5 h-5 text-yellow-400"
+										>
+											<path d="M12 17.27l5.18 3.15-1.39-6.06L22 9.24l-6.19-.53L12 2 8.19 8.71 2 9.24l4.21 4.12-1.39 6.06L12 17.27z" />
+										</svg>
 									</div>
+									<span className="text-gray-600">
+										(123 verified ratings)
+									</span>
+								</div>
+
+								{/* leave a review */}
+								<div className="relative">
+									<input
+										type="text"
+										placeholder="Leave a review"
+										id=""
+										className="border-2 border-gray-300 rounded-xl p-4 w-full focus:outline-none focus:border-blue-500"
+									/>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										fill="currentColor"
+										className="size-6 text-black absolute right-4 top-4"
+									>
+										<path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
+									</svg>
+								</div>
+							</div>
+
+							{/* show review list here */}
+
+							<div className="flex flex-col gap-2 w-full">
+								<span className="text-gray-800 h-14 px-2 mt-2 border-1 bg-[#ffffff6b] border-gray-400 rounded-sm overflow-scroll">
+									@precious{" "}
+									<span className="text-gray-500 px-1">
+										•
+									</span>
+									Today, 12:30 PM
+									<br />
+									<p className="font-light text-gray-700 leading-tight">
+										Lorem ipsum dolor sit amet consectetur
+										adipisicing elit.
+									</p>
+								</span>
+								<hr />
+								<span className="text-gray-800 h-14 px-2 mt-2 border-1 bg-[#ffff] border-gray-400 rounded-sm overflow-scroll">
+									@christian{" "}
+									<span className="text-gray-500 px-1">
+										•
+									</span>
+									Today, 12:30 PM
+									<br />
+									<p className="font-light text-gray-700 leading-tight">
+										some review here
+									</p>
+								</span>
+								<hr />
+								<span className="text-gray-800 h-14 px-2 mt-2 border-1 bg-[#ffff] border-gray-400 rounded-sm overflow-scroll">
+									@selase{" "}
+									<span className="text-gray-500 px-1">
+										•
+									</span>
+									Today, 12:30 PM
+									<br />
+									<p className="font-light text-gray-700 leading-tight">
+										user review here
+									</p>
+								</span>
+								<hr />
+
+								{/* show more */}
+								<div className="text-center">
+									<a
+										href="#"
+										className="text-blue-500 underline"
+									>
+										Show more reviews...
+									</a>
 								</div>
 							</div>
 						</div>
@@ -122,4 +246,4 @@ const ServiceProvidersProfile = () => {
 	);
 };
 
-export default ServiceProvidersProfile;
+export default ServiceProviders;
