2c6bcff953f9bbd22498d35178d77a4e350611e4
juliusmarkwei <juliusmarkwei2000@gmail.com>
2024-08-30 16:43:33 +0000

    image upload on signup

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,4 +1,22 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+
 const SignUp = () => {
+	const [avatar, setAvatar] = useState<string | ArrayBuffer | null>();
+
+	const handleFileChange = (event: any) => {
+		const file = event.target.files[0];
+		if (file) {
+			const reader = new FileReader();
+			reader.onloadend = () => {
+				setAvatar(reader.result);
+			};
+			reader.readAsDataURL(file);
+		}
+	};
+
 	return (
 		<>
 			<section className="bg-white h-screen">
@@ -17,22 +35,33 @@ const SignUp = () => {
 											name="profilePicture"
 											id="profilePicture"
 											className="absolute w-full h-full opacity-0 cursor-pointer"
+											onChange={handleFileChange}
 										/>
 										<div className="w-full h-full rounded-full overflow-hidden border-[5px] border-blue-400 flex items-center justify-center bg-gray-200 text-gray-400">
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												fill="none"
-												viewBox="0 0 24 24"
-												strokeWidth="1.5"
-												stroke="currentColor"
-												className="w-16 h-16 text-blue-300"
-											>
-												<path
-													strokeLinecap="round"
-													strokeLinejoin="round"
-													d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+											{avatar ? (
+												<Image
+													src={avatar as string}
+													alt="Avatar"
+													className="w-full h-full object-cover"
+													height={112}
+													width={112}
 												/>
-											</svg>
+											) : (
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													fill="none"
+													viewBox="0 0 24 24"
+													strokeWidth="1.5"
+													stroke="currentColor"
+													className="w-16 h-16 text-blue-300"
+												>
+													<path
+														strokeLinecap="round"
+														strokeLinejoin="round"
+														d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+													/>
+												</svg>
+											)}
 										</div>
 									</div>
 								</div>
